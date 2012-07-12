a14f0162c4fbdf3f584805e8d6b2a6e60a9f9fab
Stefan Trauth <mail@stefantrauth.de>
2012-07-12 18:08:11 +0200

    doing some doku

--- twolor.ino
+++ twolor.ino
@@ -7,33 +7,30 @@
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
-byte mac[] = { 
-  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01 };
-IPAddress ip(192,168,1,20);
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01 };
+IPAddress ip(192,168,178,120); // that is actually fallback if no dhcp available
 
 // initialize the library instance:
 EthernetClient client;
 
-const unsigned long requestInterval = 60000;  // delay between requests
+const unsigned long requestInterval = 60000; // delay between requests
+char serverName[] = "api.twitter.com"; // twitter api URL
 
-char serverName[] = "api.twitter.com";  // twitter URL
+boolean requested; // whether you've made a request since connecting
+unsigned long lastAttemptTime = 0; // last time you connected to the server, in milliseconds
 
-boolean requested;                   // whether you've made a request since connecting
-unsigned long lastAttemptTime = 0;            // last time you connected to the server, in milliseconds
-
-String currentLine = "";            // string to hold the text from server
-boolean textFound = false;
+String currentLine = ""; // string to hold the text from server
+boolean textFound = false; // trigger to check whether the "text" was found in json yet
 
 void setup() {
-  // reserve space for the strings:
-  currentLine.reserve(128);
+  // reserve space for the strings
+  currentLine.reserve(200);
 
- // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ; // wait for serial port to connect.
   }
-
 
   // attempt a DHCP connection:
   Serial.println("Attempting to get an IP address using DHCP:");
@@ -48,10 +45,7 @@ void setup() {
   connectToServer();
 }
 
-
-
-void loop()
-{
+void loop() {
   if (client.connected()) {
     if (client.available()) {
       
@@ -60,16 +54,21 @@ void loop()
       Serial.print(inChar);
       currentLine += inChar;
       
+      // check if the currently cached data from server contains the json element "text"
+      // we are interested in this element as it contains the tweet we are looking for
       if(currentLine.indexOf("\"text\":\"") > -1) {
-        textFound = true; // text found
+        textFound = true; // "text" element was found in json from server
       } 
       
+      // check if currently cached data contains the "text" tag AND if we have already received the whole tweet message
       if(currentLine.endsWith("\",") && textFound) {
+        // we received the whole tweet \o/
         Serial.println("");
+        // cut off the "text": at the beginning and the ", at the end. then print the tweet to serial
         Serial.println(currentLine.substring(8, currentLine.length() - 2));
-        client.stop();     
-        currentLine = "";
-        textFound = false;
+        client.stop(); // we received what we were interested in, so close the connection
+        currentLine = ""; // clear data
+        textFound = false; // reset triggers
         Serial.println("Will sleep now...");
       } 
       
@@ -86,12 +85,16 @@ void loop()
   }
 }
 
+/*
+ * connect to twitter api server and send a GET request
+ */
 void connectToServer() {
   // attempt to connect, and wait a millisecond:
   Serial.println("connecting to server...");
   if (client.connect(serverName, 80)) {
     Serial.println("making HTTP request...");
     // make HTTP GET request to twitter:
+    // this is the important request to receive the correct data in json format from twitter api
     client.println("GET /search.json?q=%23arduino&rpp=1&include_entities=false&result_type=recent HTTP/1.1");
     client.println("HOST: search.twitter.com");
     client.println();
